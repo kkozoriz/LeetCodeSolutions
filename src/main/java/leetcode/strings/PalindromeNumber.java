bc96e1e112c640fdacd4128a5361ac96c0b7a458
kkozoriz <kkozoriz7@yandex.ru>
2023-12-15 19:20:38 +0300

    Add solution for Palindrome Number

---
+++ src/main/java/leetcode/strings/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package leetcode.strings;
+
+//TODO: Given an integer x, return true if x is a palindrome, and false otherwise.
+//  Input: x = 121
+//  Output: true
+//  Explanation: 121 reads as 121 from left to right and from right to left.
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        String string = Integer.toString(x);
+        StringBuilder reverseString = new StringBuilder(string);
+        return string.contentEquals(reverseString.reverse());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+}
